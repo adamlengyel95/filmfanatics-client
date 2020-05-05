d2b3150de3953863413767bb2e0763aeef692083
9adam <lengyel.adam95@gmail.com>
2020-05-05 22:13:17 +0200

    Proxy changes2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = app => {
-  app.use(createProxyMiddleware("/*", { target: "https://filmfanatics-api.herokuapp.com/" }));
+  app.use(createProxyMiddleware("/*", { target: "https://filmfanatics-api.herokuapp.com/", changeOrigin: true, pathRewrite: { '^/': '' } }));
 };
