52b926e89a3aa6a366ffe136cdd4c7e58d12dff1
9adam <lengyel.adam95@gmail.com>
2020-05-06 21:31:00 +0200

    updated base url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 const constants = {
     API_URL: 'http://filmfanatics-api.rabit.hu',
-    BASE_URL: 'http://localhost:3000'
+    BASE_URL: 'http://filmfanatics-client.rabit.hu/'
 }
 
 export default constants;
